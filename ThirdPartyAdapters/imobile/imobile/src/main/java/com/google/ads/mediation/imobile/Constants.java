f5e04fdebc41069b768e1b897f8bfe95cbfa8e04
Jun-ya HASEBA <7pairs@gmail.com>
2019-05-13 16:42:13 +0900

    Add i-mobile adapter

---
+++ ThirdPartyAdapters/imobile/imobile/src/main/java/com/google/ads/mediation/imobile/Constants.java
@@ -0,0 +1,19 @@
+package com.google.ads.mediation.imobile;
+
+/**
+ * Constants for i-mobile mediation adapter.
+ */
+public interface Constants {
+
+    /** Key of i-mobile publisher ID. */
+    String KEY_PUBLISHER_ID = "publisherId";
+
+    /** Key of i-mobile media ID. */
+    String KEY_MEDIA_ID = "mid";
+
+    /** Key of i-mobile spot ID. */
+    String KEY_SPOT_ID = "asid";
+
+    /** String of call to action. */
+    String CALL_TO_ACTION = "詳細はこちら";
+}
---
+++ ThirdPartyAdapters/imobile/imobile/src/main/java/com/google/ads/mediation/imobile/IMobileUnifiedNativeAdMapper.java
@@ -0,0 +1,65 @@
+package com.google.ads.mediation.imobile;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.view.View;
+
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.ads.mediation.UnifiedNativeAdMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.co.imobile.sdkads.android.ImobileSdkAdsNativeAdData;
+
+/**
+ * Mapper for UnifiedNativeAd.
+ */
+public final class IMobileUnifiedNativeAdMapper extends UnifiedNativeAdMapper {
+
+    /** Called when clicked. */
+    private Runnable clickEvent;
+
+    /** Ad image. */
+    private Drawable adDrawable;
+
+    public IMobileUnifiedNativeAdMapper(ImobileSdkAdsNativeAdData adData, Bitmap adImage) {
+        // Initialize fields.
+        this.clickEvent = adData.getClickEvent();
+        this.adDrawable = new BitmapDrawable(adImage);
+
+        // Set ad image.
+        List<NativeAd.Image> images = new ArrayList<>(1);
+        images.add(new NativeAd.Image() {
+            @Override
+            public Drawable getDrawable() {
+                return adDrawable;
+            }
+
+            @Override
+            public Uri getUri() {
+                return null;
+            }
+
+            @Override
+            public double getScale() {
+                return 1;
+            }
+        });
+        setImages(images);
+
+        // Set ad data.
+        setAdvertiser(adData.getSponsored());
+        setBody(adData.getDescription());
+        setCallToAction(Constants.CALL_TO_ACTION);
+        setHeadline(adData.getTitle());
+    }
+
+    @Override
+    public void handleClick(View view) {
+        // Run click event.
+        clickEvent.run();
+    }
+}
