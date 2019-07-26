cd68261aa3f160bf50b77352ffc226db3fdba62b
Smriti Raj <smriti.raj@inmobi.com>
2019-07-26 16:15:25 +0530

    Upgrading to androidx

--- ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/InMobiAdapterUtils.java
+++ ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/InMobiAdapterUtils.java
@@ -3,7 +3,7 @@ package com.google.ads.mediation.inmobi;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.google.android.gms.ads.AdRequest;
--- ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/rtb/InMobiBannerAd.java
+++ ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/rtb/InMobiBannerAd.java
@@ -1,7 +1,7 @@
 package com.google.ads.mediation.inmobi.rtb;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 
