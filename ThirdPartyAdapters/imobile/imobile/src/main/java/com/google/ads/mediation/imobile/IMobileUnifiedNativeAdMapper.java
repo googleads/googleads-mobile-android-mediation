4d29d4d2e503ae2b56c17662957e9abcfb232fef
Sai Avinash Reddy Pilaka <spilaka@google.com>
2019-08-22 13:47:15 -0700

    Updated i-mobile Adapter Native AdIcon with a transparent drawable.
    PiperOrigin-RevId: 264910599

--- ThirdPartyAdapters/imobile/imobile/src/main/java/com/google/ads/mediation/imobile/IMobileUnifiedNativeAdMapper.java
+++ ThirdPartyAdapters/imobile/imobile/src/main/java/com/google/ads/mediation/imobile/IMobileUnifiedNativeAdMapper.java
@@ -1,5 +1,7 @@
 package com.google.ads.mediation.imobile;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.view.View;
@@ -20,32 +22,13 @@ public final class IMobileUnifiedNativeAdMapper extends UnifiedNativeAdMapper {
     /** Called when clicked. */
     private Runnable clickEvent;
 
-    /** Ad image. */
-    private Drawable adDrawable;
-
     public IMobileUnifiedNativeAdMapper(ImobileSdkAdsNativeAdData adData, Drawable adImage) {
         // Initialize fields.
         this.clickEvent = adData.getClickEvent();
-        this.adDrawable = adImage;
 
         // Set ad image.
         List<NativeAd.Image> images = new ArrayList<>(1);
-        images.add(new NativeAd.Image() {
-            @Override
-            public Drawable getDrawable() {
-                return adDrawable;
-            }
-
-            @Override
-            public Uri getUri() {
-                return null;
-            }
-
-            @Override
-            public double getScale() {
-                return 1;
-            }
-        });
+        images.add(new NativeAdImage(adImage, null, 1));
         setImages(images);
 
         // Set ad data.
@@ -53,11 +36,42 @@ public final class IMobileUnifiedNativeAdMapper extends UnifiedNativeAdMapper {
         setBody(adData.getDescription());
         setCallToAction(Constants.CALL_TO_ACTION);
         setHeadline(adData.getTitle());
+
+        //Created a transparent drawable as i-mobile do not render AdIcon.
+        setIcon(new NativeAdImage(new ColorDrawable(Color.TRANSPARENT), null, 1));
     }
 
     @Override
     public void handleClick(View view) {
         // Run click event.
         clickEvent.run();
+    }
+
+    class NativeAdImage extends NativeAd.Image {
+
+        private final Drawable drawable;
+        private final Uri imageUri;
+        private final double scale;
+
+        NativeAdImage(Drawable drawable, Uri imageUri, double scale) {
+            this.drawable = drawable;
+            this.imageUri = imageUri;
+            this.scale = scale;
+        }
+
+        @Override
+        public Drawable getDrawable() {
+            return drawable;
+        }
+
+        @Override
+        public Uri getUri() {
+            return imageUri;
+        }
+
+        @Override
+        public double getScale() {
+            return scale;
+        }
     }
 }
