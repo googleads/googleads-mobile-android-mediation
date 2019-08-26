d4deb512152aa4bd342777bb29a726fc3f271180
Rajesh Mergu <rajesh.mergu@adcolony.com>
2019-08-26 17:12:13 +0530

    Added callback support for banner ads.

--- ThirdPartyAdapters/adcolony/adcolony/src/main/java/com/google/ads/mediation/adcolony/AdColonyBannerRenderer.java
+++ ThirdPartyAdapters/adcolony/adcolony/src/main/java/com/google/ads/mediation/adcolony/AdColonyBannerRenderer.java
@@ -19,7 +19,8 @@ public class AdColonyBannerRenderer extends AdColonyAdViewListener implements Me
 
     private final String requestedZone;
     private final AdColonyAdSize adSize;
-    private MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> mAdLoadCallback;
+    private MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> mediationAdLoadCallback;
+    private MediationBannerAdCallback mAdLoadCallback;
     private AdColonyAdView adColonyAdView;
 
     public AdColonyBannerRenderer(MediationBannerAdConfiguration adConfiguration) {
@@ -27,24 +28,37 @@ public class AdColonyBannerRenderer extends AdColonyAdViewListener implements Me
         // Convert requested size to AdColony Ad Size.
         this.adSize = AdColonyAdapterUtils.adColonyAdSizeFromAdMobAdSize(
                 adConfiguration.getContext(), adConfiguration.getAdSize());
-
     }
 
     @Override
     public void onRequestFilled(AdColonyAdView adColonyAdView) {
         this.adColonyAdView = adColonyAdView;
-        this.mAdLoadCallback.onSuccess(this);
-    }
-
-    @Override
-    public void onClicked(AdColonyAdView ad) {
-        super.onClicked(ad);
+        this.mAdLoadCallback = mediationAdLoadCallback.onSuccess(this);
     }
 
     @Override
     public void onRequestNotFilled(AdColonyZone zone) {
-        super.onRequestNotFilled(zone);
-        this.mAdLoadCallback.onFailure("Failed to load banner.");
+        this.mediationAdLoadCallback.onFailure("Failed to load banner.");
+    }
+
+    @Override
+    public void onLeftApplication(AdColonyAdView ad) {
+        this.mAdLoadCallback.onAdLeftApplication();
+    }
+
+    @Override
+    public void onClosed(AdColonyAdView ad) {
+        this.mAdLoadCallback.onAdClosed();
+    }
+
+    @Override
+    public void onOpened(AdColonyAdView ad) {
+        this.mAdLoadCallback.onAdOpened();
+    }
+
+    @Override
+    public void onClicked(AdColonyAdView ad) {
+        this.mAdLoadCallback.reportAdClicked();
     }
 
     @NonNull
@@ -55,7 +69,7 @@ public class AdColonyBannerRenderer extends AdColonyAdViewListener implements Me
 
     public void requestBanner(MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> mediationAdLoadCallback) {
         if (adSize != null) {
-            this.mAdLoadCallback = mediationAdLoadCallback;
+            this.mediationAdLoadCallback = mediationAdLoadCallback;
             AdColony.requestAdView(requestedZone, this, adSize);
         } else {
             mediationAdLoadCallback.onFailure("Failed to request banner with unsupported size");
