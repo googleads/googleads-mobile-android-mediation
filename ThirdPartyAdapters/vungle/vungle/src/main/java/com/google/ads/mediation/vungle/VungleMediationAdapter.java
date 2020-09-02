9815f0cd0128e5ab3fe8f447b1e734baa5336fba
Joshua James Pascual Lagonera <joshuaja@google.com>
2020-09-02 15:33:25 -0700

    Updating Vungle Android adapter for release version 6.7.1.0
    PiperOrigin-RevId: 329801237

--- ThirdPartyAdapters/vungle/vungle/src/main/java/com/google/ads/mediation/vungle/VungleMediationAdapter.java
+++ ThirdPartyAdapters/vungle/vungle/src/main/java/com/google/ads/mediation/vungle/VungleMediationAdapter.java
@@ -187,7 +187,7 @@ public class VungleMediationAdapter extends Adapter
     VungleInitializer.getInstance()
         .initialize(
             appID,
-            context.getApplicationContext(),
+            mediationRewardedAdConfiguration.getContext(),
             new VungleInitializationListener() {
               @Override
               public void onInitializeSuccess() {
