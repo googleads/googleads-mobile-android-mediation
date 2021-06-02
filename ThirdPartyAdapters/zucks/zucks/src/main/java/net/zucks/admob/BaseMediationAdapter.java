e07c05a4f5713a7c04c89ca92888c439726e2e55
Shuma Yoshioka <shuma_yoshioka@voyagegroup.com>
2021-06-02 16:19:05 +0900

    Copy BaseMediationAdapter

---
+++ ThirdPartyAdapters/zucks/zucks/src/main/java/net/zucks/admob/BaseMediationAdapter.java
@@ -0,0 +1,36 @@
+package net.zucks.admob;
+
+import android.content.Context;
+
+import androidx.annotation.StringDef;
+
+import com.google.android.gms.ads.mediation.Adapter;
+import com.google.android.gms.ads.mediation.InitializationCompleteCallback;
+import com.google.android.gms.ads.mediation.MediationConfiguration;
+import com.google.android.gms.ads.mediation.VersionInfo;
+import java.util.List;
+
+/**
+ * Base class for Zucks Mediation Adapters.
+ * Provide common logic for compatibility.
+ */
+public abstract class BaseMediationAdapter extends Adapter {
+
+    @Override
+    public void initialize(
+            Context context,
+            InitializationCompleteCallback initializationCompleteCallback,
+            List<MediationConfiguration> list) {
+        initializationCompleteCallback.onInitializationSucceeded();
+    }
+
+    @Override
+    public VersionInfo getVersionInfo() {
+        return AdMobUtil.getAdapterVersionInfo();
+    }
+
+    @Override
+    public VersionInfo getSDKVersionInfo() {
+        return AdMobUtil.getNetworkSdkVersionInfo();
+    }
+}
