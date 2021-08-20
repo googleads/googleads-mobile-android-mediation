46fad040168a6e55b78124b5d66cfc9513dac895
Eric Leichtenschlag <eleichtenschl@google.com>
2021-08-20 10:31:34 -0700

    Internal change.
    PiperOrigin-RevId: 392019919

--- ThirdPartyAdapters/mytarget/build_adapter.sh
+++ ThirdPartyAdapters/mytarget/build_adapter.sh
@@ -4,7 +4,7 @@
 # 1. Requires a fig client
 # 2. Requires gcert
 
-source "/google/src/head/depot/third_party/java_src/gma_sdk_mediation/scripts/build_adapter_common.sh"
+source "/google/src/head/depot/google3/third_party/java_src/gma_sdk_mediation/scripts/build_adapter_common.sh"
 # Uncomment for testing
 # GOOGLE3=$(pwd | sed "s,^\(.*\)google3.*$,\1google3,")
 # source "${GOOGLE3}/third_party/java_src/gma_sdk_mediation/scripts/build_adapter_common.sh"
