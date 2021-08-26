150eee8c09240d3d6a2a0229caa654ba453b9071
Mobile Ads Developer Relations <mad-devrel@google.com>
2021-08-26 14:26:00 -0700

    Updating Adcolony Android Adapter to version 6.10.1.0 in compliance with SDK changes.
    PiperOrigin-RevId: 393206797

--- ThirdPartyAdapters/vungle/vungle/src/main/java/com/vungle/mediation/VungleAdapter.java
+++ ThirdPartyAdapters/vungle/vungle/src/main/java/com/vungle/mediation/VungleAdapter.java
@@ -9,4 +9,6 @@ import com.google.android.gms.ads.mediation.MediationAdapter;
  * mediation.
  */
 @Keep
-public class VungleAdapter extends VungleMediationAdapter {}
+public class VungleAdapter extends VungleMediationAdapter {
+
+}
