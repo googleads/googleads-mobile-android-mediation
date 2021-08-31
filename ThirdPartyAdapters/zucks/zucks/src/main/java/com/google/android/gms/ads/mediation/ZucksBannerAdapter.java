be345b78640ae7951ccd9e5d85e8101a1cec2f86
Shuma Yoshioka <shuma_yoshioka@voyagegroup.com>
2021-08-31 17:57:13 +0900

    Flatten ZucksBannerAdapter

--- ThirdPartyAdapters/zucks/zucks/src/main/java/com/google/android/gms/ads/mediation/ZucksBannerAdapter.java
+++ ThirdPartyAdapters/zucks/zucks/src/main/java/com/google/android/gms/ads/mediation/ZucksBannerAdapter.java
@@ -30,151 +30,106 @@ import java.util.List;
 class ZucksBannerAdapter extends ZucksMediationAdapter
     implements MediationBannerAd {
 
-  /** New adapter implementation */
-  @VisibleForTesting
-  static class ZucksMediationBannerAd implements MediationBannerAd {
+  @NonNull private final MediationBannerAd root;
 
-    @NonNull private final ZucksBannerAdapter self;
+  @NonNull private final Context context;
 
-    @Nullable
-    private MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> loadCallback;
+  @Nullable private final AdSize adSize;
 
-    @Nullable private MediationBannerAdCallback adCallback = null;
+  @NonNull private final Bundle serverParams;
 
-    @VisibleForTesting @NonNull
-    final AdBannerListener listener =
-        new AdBannerListener() {
+  @NonNull
+  private final MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> loadCallback;
 
-          @Override
-          public void onReceiveAd(AdBanner banner) {
-            AdError error = self.isValidAdSize(banner);
-            if (error != null) {
-              loadCallback.onFailure(error);
-            } else {
-              adCallback = loadCallback.onSuccess(self.root);
-              adCallback.reportAdImpression();
-            }
-          }
-
-          @Override
-          public void onFailure(AdBanner banner, Exception e) {
-            loadCallback.onFailure(ErrorMapper.convertSdkError(e));
-          }
-
-          @Override
-          public void onTapAd(AdBanner banner) {
-            adCallback.reportAdClicked();
-            adCallback.onAdOpened();
-            adCallback.onAdLeftApplication();
-          }
-
-          @Override
-          public void onBackApplication(AdBanner banner) {
-            adCallback.onAdClosed();
-          }
-        };
-
-    @VisibleForTesting
-    ZucksMediationBannerAd(@NonNull ZucksBannerAdapter self) {
-      this.self = self;
-    }
-
-    @VisibleForTesting
-    void loadBannerAd(
-        @NonNull MediationBannerAdConfiguration configuration,
-        @NonNull
-            MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback>
-                mediationAdLoadCallback) {
-      setLoadCallback(mediationAdLoadCallback);
-
-      AdError error =
-          self.configureBannerAd(
-              configuration.getContext(),
-              configuration.getAdSize(),
-              configuration.getServerParameters(),
-              listener);
-      if (error != null) {
-        loadCallback.onFailure(error);
-      }
-    }
-
-    @VisibleForTesting
-    void setLoadCallback(
-        @NonNull MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> callback) {
-      this.loadCallback = callback;
-    }
-
-    @NonNull
-    @Override
-    public View getView() {
-      return self.zucksBanner;
-    }
-  }
-
-  @NonNull private final ZucksAdapter root;
+  @Nullable private MediationBannerAdCallback adCallback = null;
 
   /** Banner instance of Zucks Ad Network SDK. */
   @Nullable private AdBanner zucksBanner = null;
 
-  /** Size instance of Zucks Ad Network SDK. */
-  @Nullable private AdSize adSize = null;
+  @VisibleForTesting @NonNull
+  final AdBannerListener listener =
+          new AdBannerListener() {
 
-  // region New adapter
-  @Nullable private ZucksMediationBannerAd mediationBannerAd = null;
+            @Override
+            public void onReceiveAd(AdBanner banner) {
+              AdError error = isValidAdSize(banner);
+              if (error != null) {
+                loadCallback.onFailure(error);
+              } else {
+                adCallback = loadCallback.onSuccess(root);
+                adCallback.reportAdImpression();
+              }
+            }
 
-  @NonNull
-  private ZucksMediationBannerAd useBannerAd() {
-    if (mediationBannerAd == null) {
-      mediationBannerAd = new ZucksMediationBannerAd(this);
-    }
-    return mediationBannerAd;
-  }
-  // endregion
+            @Override
+            public void onFailure(AdBanner banner, Exception e) {
+              loadCallback.onFailure(ErrorMapper.convertSdkError(e));
+            }
 
-  ZucksBannerAdapter(@NonNull ZucksAdapter root) {
+            @Override
+            public void onTapAd(AdBanner banner) {
+              adCallback.reportAdClicked();
+              adCallback.onAdOpened();
+              adCallback.onAdLeftApplication();
+            }
+
+            @Override
+            public void onBackApplication(AdBanner banner) {
+              adCallback.onAdClosed();
+            }
+          };
+
+  ZucksBannerAdapter(
+          @NonNull MediationBannerAd root,
+          @NonNull MediationBannerAdConfiguration configuration,
+          @NonNull MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> mediationAdLoadCallback) {
     this.root = root;
-  }
-
-  @Override
-  public void loadBannerAd(
-      MediationBannerAdConfiguration mediationBannerAdConfiguration,
-      MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback>
-          mediationAdLoadCallback) {
-    useBannerAd().loadBannerAd(mediationBannerAdConfiguration, mediationAdLoadCallback);
+    this.context = configuration.getContext();
+    this.adSize = configuration.getAdSize();
+    this.serverParams = configuration.getServerParameters();
+    this.loadCallback = mediationAdLoadCallback;
   }
 
   @VisibleForTesting
-  @Nullable
-  AdError configureBannerAd(
-      Context context, AdSize adSize, Bundle serverParams, AdBannerListener listener) {
+  void loadBannerAd() {
+    String adFrameId;
+
     if (!isSizeSupported(context, adSize)) {
-      return ErrorMapper.createAdapterError(
-          ErrorMapper.ADAPTER_ERROR_INVALID_REQUEST, "It is not a supported size. size=" + adSize);
+      loadCallback.onFailure(
+              ErrorMapper.createAdapterError(
+                      ErrorMapper.ADAPTER_ERROR_INVALID_REQUEST,
+                      "It is not a supported size. size=" + adSize)
+      );
+      return;
     }
 
-    String adFrameId = AdMobUtil.getFrameId(serverParams);
-
-    if (adFrameId == null) {
-      return ErrorMapper.createAdapterError(
-          ErrorMapper.ADAPTER_ERROR_INVALID_REQUEST, "FrameID not contained in serverParameters.");
-    } else {
-      this.adSize = adSize;
-      zucksBanner = new AdBanner(context, adFrameId, listener);
-      AdMobUtil.configurePlatform(zucksBanner);
-      zucksBanner.load();
-      return null;
+    if ((adFrameId = AdMobUtil.getFrameId(serverParams)) == null) {
+      loadCallback.onFailure(
+              ErrorMapper.createAdapterError(
+                      ErrorMapper.ADAPTER_ERROR_INVALID_REQUEST,
+                      "FrameID not contained in serverParameters.")
+      );
+      return;
     }
+
+    zucksBanner = new AdBanner(context, adFrameId, listener);
+    AdMobUtil.configurePlatform(zucksBanner);
+    zucksBanner.load();
   }
 
   @NonNull
   @Override
   public View getView() {
-    return useBannerAd().getView();
+    return zucksBanner;
   }
 
   /** Validate passed size are supported in Zucks Ad Network SDK. */
   @VisibleForTesting
-  static boolean isSizeSupported(@NonNull Context context, @NonNull AdSize adSize) {
+  static boolean isSizeSupported(@NonNull Context context, @Nullable AdSize adSize) {
+    if (adSize == null) {
+      return false;
+    }
+
     List<AdSize> supported = new ArrayList<>();
 
     supported.add(AdSize.BANNER);
@@ -194,8 +149,8 @@ class ZucksBannerAdapter extends ZucksMediationAdapter
   /** For internal assertion. Validate passed size and actual size are equals. */
   @VisibleForTesting
   @Nullable
-  static AdError isValidAdSize(@NonNull AdSize adSize, @NonNull AdBanner banner) {
-    if (adSize.getWidth() != banner.getWidthInDp()
+  static AdError isValidAdSize(@Nullable AdSize adSize, @NonNull AdBanner banner) {
+    if (adSize == null || adSize.getWidth() != banner.getWidthInDp()
         || adSize.getHeight() != banner.getHeightInDp()) {
       return ErrorMapper.createAdapterError(
           ErrorMapper.ADAPTER_ERROR_ILLEGAL_STATE, "It is not a supported size. size=" + adSize);
@@ -203,4 +158,5 @@ class ZucksBannerAdapter extends ZucksMediationAdapter
       return null;
     }
   }
+
 }
