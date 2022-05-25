ba72b0c83fe9e3c0103e4fd0989282ef35dcdaef
HuFangshuai <fangshuai.hu@vungle.com>
2022-05-25 10:38:15 +0800

    [AND-4088] Bugfix: gdpr no effect for the config request. (#131)

--- ThirdPartyAdapters/vungle/vungle/src/main/java/com/vungle/mediation/VungleConsent.java
+++ ThirdPartyAdapters/vungle/vungle/src/main/java/com/vungle/mediation/VungleConsent.java
@@ -7,29 +7,12 @@ import com.vungle.warren.Vungle;
  */
 public class VungleConsent {
 
-  private static Vungle.Consent sCurrentVungleConsent = null;
-  private static String sCurrentVungleConsentMessageVersion = "";
-
   /**
    * Update GDPR consent status and corresponding version number.
    */
   public static void updateConsentStatus(
       Vungle.Consent consentStatus, String consentMessageVersion) {
-    sCurrentVungleConsent = consentStatus;
-    sCurrentVungleConsentMessageVersion = consentMessageVersion;
-
-    if (Vungle.isInitialized()
-        && sCurrentVungleConsent != null
-        && sCurrentVungleConsentMessageVersion != null) {
-      Vungle.updateConsentStatus(sCurrentVungleConsent, sCurrentVungleConsentMessageVersion);
-    }
+    Vungle.updateConsentStatus(consentStatus, consentMessageVersion);
   }
 
-  public static Vungle.Consent getCurrentVungleConsent() {
-    return sCurrentVungleConsent;
-  }
-
-  public static String getCurrentVungleConsentMessageVersion() {
-    return sCurrentVungleConsentMessageVersion;
-  }
 }
