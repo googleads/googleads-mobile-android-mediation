de0b8adb1d9805d7e7dc6264ab7909056484b2db
张科通 <zhangketong@bytedance.com>
2022-09-09 08:29:51 +0800

    [fix]Optimize code logic

--- ThirdPartyAdapters/pangle/pangle/src/main/java/com/google/ads/mediation/pangle/PangleMediationAdapter.java
+++ ThirdPartyAdapters/pangle/pangle/src/main/java/com/google/ads/mediation/pangle/PangleMediationAdapter.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import com.bytedance.sdk.openadsdk.api.PAGConstant.PAGDoNotSellType;
 import com.bytedance.sdk.openadsdk.api.PAGConstant.PAGGDPRConsentType;
 import com.bytedance.sdk.openadsdk.api.init.PAGConfig;
@@ -53,7 +52,10 @@ public class PangleMediationAdapter extends RtbAdapter {
   @Override
   public void collectSignals(
       @NonNull RtbSignalData rtbSignalData, @NonNull SignalCallbacks signalCallbacks) {
-    setUserData(rtbSignalData.getNetworkExtras());
+    Bundle networkExtras = rtbSignalData.getNetworkExtras();
+    if (networkExtras.containsKey(PangleExtras.Keys.USER_DATA)) {
+      PAGConfig.setUserData(networkExtras.getString(PangleExtras.Keys.USER_DATA, ""));
+    }
     String biddingToken = PAGSdk.getBiddingToken();
     signalCallbacks.onSuccess(biddingToken);
   }
@@ -229,18 +231,5 @@ public class PangleMediationAdapter extends RtbAdapter {
       PAGConfig.setDoNotSell(ccpa);
     }
     PangleMediationAdapter.ccpa = ccpa;
-  }
-
-  /**
-   * Set the user data (e.g. in-app purchase status) to be sent to Pangle SDK.
-   *
-   * @param networkExtras a {@link Bundle} containing optional parameter to be passed to the
-   *                      adapter.
-   */
-  private static void setUserData(@Nullable Bundle networkExtras) {
-    if (networkExtras == null || !networkExtras.containsKey(PangleExtras.Keys.USER_DATA)) {
-      return;
-    }
-    PAGConfig.setUserData(networkExtras.getString(PangleExtras.Keys.USER_DATA, ""));
   }
 }
