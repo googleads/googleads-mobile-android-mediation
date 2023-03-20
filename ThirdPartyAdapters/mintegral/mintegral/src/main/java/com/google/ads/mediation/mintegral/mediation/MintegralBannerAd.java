ce0d1bad199fe9be1c8164121aae26956069c9cd
Joshua James Pascual Lagonera <joshuaja@google.com>
2023-03-20 14:55:16 -0700

    Updated License headers for the Mintegral Android adapter
    PiperOrigin-RevId: 518084882

--- ThirdPartyAdapters/mintegral/mintegral/src/main/java/com/google/ads/mediation/mintegral/mediation/MintegralBannerAd.java
+++ ThirdPartyAdapters/mintegral/mintegral/src/main/java/com/google/ads/mediation/mintegral/mediation/MintegralBannerAd.java
@@ -1,3 +1,17 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.ads.mediation.mintegral.mediation;
 
 import static com.google.ads.mediation.mintegral.MintegralMediationAdapter.TAG;
@@ -5,9 +19,7 @@ import static com.google.ads.mediation.mintegral.MintegralMediationAdapter.TAG;
 import android.content.Context;
 import android.util.Log;
 import android.view.View;
-
 import androidx.annotation.NonNull;
-
 import androidx.annotation.Nullable;
 import com.google.ads.mediation.mintegral.MintegralConstants;
 import com.google.android.gms.ads.AdError;
@@ -18,7 +30,6 @@ import com.google.android.gms.ads.mediation.MediationBannerAdConfiguration;
 import com.mbridge.msdk.out.BannerAdWithCodeListener;
 import com.mbridge.msdk.out.MBBannerView;
 import com.mbridge.msdk.out.MBridgeIds;
-
 
 public abstract class MintegralBannerAd extends BannerAdWithCodeListener implements MediationBannerAd {
 
--- ThirdPartyAdapters/mintegral/mintegral/src/main/java/com/google/ads/mediation/mintegral/waterfall/MintegralWaterfallNativeAd.java
+++ ThirdPartyAdapters/mintegral/mintegral/src/main/java/com/google/ads/mediation/mintegral/waterfall/MintegralWaterfallNativeAd.java
@@ -1,11 +1,23 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.ads.mediation.mintegral.waterfall;
 
 import static com.mbridge.msdk.MBridgeConstans.NATIVE_VIDEO_SUPPORT;
 
 import android.view.View;
-
 import androidx.annotation.NonNull;
-
 import com.google.ads.mediation.mintegral.MintegralConstants;
 import com.google.ads.mediation.mintegral.MintegralUtils;
 import com.google.ads.mediation.mintegral.mediation.MintegralNativeAd;
@@ -18,7 +30,6 @@ import com.google.android.gms.ads.nativead.NativeAdAssetNames;
 import com.mbridge.msdk.MBridgeConstans;
 import com.mbridge.msdk.out.MBBidNativeHandler;
 import com.mbridge.msdk.out.MBNativeHandler;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
