1807703e6e4e292273f2d8eb7ded70536920a1d1
Joshua James Pascual Lagonera <joshuaja@google.com>
2023-03-21 11:36:49 -0700

    Updated License headers for the Yahoo Android adapter.
    PiperOrigin-RevId: 518333787

--- ThirdPartyAdapters/yahoo/yahoo/src/main/java/com/google/ads/mediation/yahoo/AdapterUnifiedNativeAdMapper.java
+++ ThirdPartyAdapters/yahoo/yahoo/src/main/java/com/google/ads/mediation/yahoo/AdapterUnifiedNativeAdMapper.java
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.ads.mediation.yahoo;
 
 import static com.google.ads.mediation.yahoo.YahooMediationAdapter.ERROR_DOMAIN;
@@ -22,12 +36,12 @@ import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.mediation.UnifiedNativeAdMapper;
 import com.yahoo.ads.nativeplacement.NativeAd;
 import com.yahoo.ads.utils.ThreadUtils;
-import org.json.JSONObject;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONObject;
 
 class AdapterUnifiedNativeAdMapper extends UnifiedNativeAdMapper {
 
