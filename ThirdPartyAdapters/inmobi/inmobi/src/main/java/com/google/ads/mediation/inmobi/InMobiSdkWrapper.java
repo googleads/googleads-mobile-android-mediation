60b407929b817747b0e4b5a310d207b0dd352514
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2023-06-14 11:28:59 -0700

    Added unit tests for InMobiInitializer.
    PiperOrigin-RevId: 540330730

--- ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/InMobiSdkWrapper.java
+++ ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/InMobiSdkWrapper.java
@@ -1,9 +1,11 @@
 package com.google.ads.mediation.inmobi;
 
+import android.content.Context;
 import androidx.annotation.NonNull;
 import com.inmobi.sdk.InMobiSdk;
-
+import com.inmobi.sdk.SdkInitializationListener;
 import java.util.Map;
+import org.json.JSONObject;
 
 /**
  A wrapper class to enable mocking the static methods on InMobi SDK for adapter unit tests.
@@ -25,5 +27,13 @@ public class InMobiSdkWrapper {
 
   public void setIsAgeRestricted(Boolean isAgeRestricted) {
     InMobiSdk.setIsAgeRestricted(isAgeRestricted);
+  }
+
+  public void init(
+      final Context context,
+      final String accountId,
+      final JSONObject consentObject,
+      final SdkInitializationListener listener) {
+    InMobiSdk.init(context, accountId, consentObject, listener);
   }
 }
