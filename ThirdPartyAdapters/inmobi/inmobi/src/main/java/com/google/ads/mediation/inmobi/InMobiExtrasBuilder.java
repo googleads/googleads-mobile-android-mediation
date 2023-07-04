89de2262f0938d97485b388afb760511613d6c7e
venkinmobi <venkimurthy87@gmail.com>
2023-07-04 20:24:57 +0530

    Added support for IAB US privacy String

--- ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/InMobiExtrasBuilder.java
+++ ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/InMobiExtrasBuilder.java
@@ -1,10 +1,16 @@
 package com.google.ads.mediation.inmobi;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.RequestConfiguration;
+import com.inmobi.compliance.InMobiPrivacyCompliance;
+
 import java.util.HashMap;
 
 public class InMobiExtrasBuilder {
@@ -15,16 +21,33 @@ public class InMobiExtrasBuilder {
   public static final String COPPA = "coppa";
 
   @NonNull
-  public static InMobiExtras build(@Nullable Bundle mediationExtras, @NonNull String protocol) {
+  public static InMobiExtras build(@NonNull Context context, @Nullable Bundle mediationExtras, @NonNull String protocol) {
     HashMap<String, String> map = new HashMap<>();
     // Set keywords as an empty string for now.
     String keywords = "";
+    boolean isIABUSStringAddedFromExtras = false;
     if (mediationExtras != null && mediationExtras.keySet() != null) {
       for (String key : mediationExtras.keySet()) {
-        map.put(key, mediationExtras.getString(key));
+        if(!key.contains(InMobiNetworkKeys.IAB_US_STRING))
+          map.put(key, mediationExtras.getString(key));
+      }
+
+      //US privacy string from Mediation Extras
+      String iabUSPrivacyString = mediationExtras.getString(InMobiNetworkKeys.IAB_US_STRING);
+      if (iabUSPrivacyString != null) {
+        InMobiPrivacyCompliance.setUSPrivacyString(iabUSPrivacyString);
+        isIABUSStringAddedFromExtras = true;
       }
     }
 
+    //US privacy string from SharedPreferences
+    if(!isIABUSStringAddedFromExtras) {
+      SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+      String iabUSPPrivacyStringFromPreferences = sharedPref.getString(InMobiNetworkKeys.IAB_US_STRING, null);
+      if (iabUSPPrivacyStringFromPreferences != null) {
+        InMobiPrivacyCompliance.setUSPrivacyString(iabUSPPrivacyStringFromPreferences);
+      }
+    }
     map.put(THIRD_PARTY_KEY, protocol);
     map.put(THIRD_PARTY_VERSION, MobileAds.getVersion().toString());
     // If the COPPA value isn't specified by the publisher, InMobi SDK expects the default value to
