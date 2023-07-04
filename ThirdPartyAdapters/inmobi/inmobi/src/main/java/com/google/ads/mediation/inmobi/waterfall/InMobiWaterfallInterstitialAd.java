1ecd2b8957b877d9117878d9f95f9ea27d4016c2
venkinmobi <venkimurthy87@gmail.com>
2023-07-04 20:40:28 +0530

    Resoled unit test cases

--- ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/waterfall/InMobiWaterfallInterstitialAd.java
+++ ThirdPartyAdapters/inmobi/inmobi/src/main/java/com/google/ads/mediation/inmobi/waterfall/InMobiWaterfallInterstitialAd.java
@@ -32,7 +32,7 @@ public class InMobiWaterfallInterstitialAd extends InMobiInterstitialAd {
   @Override
   protected void internalLoadAd(InMobiInterstitialWrapper inMobiInterstitialWrapper) {
     InMobiExtras inMobiExtras =
-        InMobiExtrasBuilder.build(
+        InMobiExtrasBuilder.build(mediationInterstitialAdConfiguration.getContext(),
             mediationInterstitialAdConfiguration.getMediationExtras(), InMobiAdapterUtils.PROTOCOL_WATERFALL);
     inMobiInterstitialWrapper.setExtras(inMobiExtras.getParameterMap());
     inMobiInterstitialWrapper.setKeywords(inMobiExtras.getKeywords());
