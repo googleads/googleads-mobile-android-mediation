411c3129976b77bd3ad37a230dbed482937bde3a
Alon Dotan <alond@is.com>
2023-07-20 15:34:57 +0300

    Fix PR comments

--- ThirdPartyAdapters/ironsource/ironsource/src/main/java/com/google/ads/mediation/ironsource/IronSourceRewardItem.java
+++ ThirdPartyAdapters/ironsource/ironsource/src/main/java/com/google/ads/mediation/ironsource/IronSourceRewardItem.java
@@ -1,3 +1,18 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// package com.google.ads.mediation.ironsource;
+
 package com.google.ads.mediation.ironsource;
 
 import androidx.annotation.NonNull;
@@ -5,14 +20,14 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.ads.rewarded.RewardItem;
 
 public class IronSourceRewardItem implements RewardItem {
-    @Override
-    public int getAmount() {
-        return 1;
-    }
+  @Override
+  public int getAmount() {
+    return 1;
+  }
 
-    @NonNull
-    @Override
-    public String getType() {
-        return "";
-    }
+  @NonNull
+  @Override
+  public String getType() {
+    return "";
+  }
 }
--- ThirdPartyAdapters/ironsource/ironsource/src/main/java/com/google/ads/mediation/ironsource/IronSourceRewardedAd.java
+++ ThirdPartyAdapters/ironsource/ironsource/src/main/java/com/google/ads/mediation/ironsource/IronSourceRewardedAd.java
@@ -1,13 +1,26 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.ads.mediation.ironsource;
 
-import static com.google.ads.mediation.ironsource.IronSourceAdapterUtils.DEFAULT_INSTANCE_ID;
-import static com.google.ads.mediation.ironsource.IronSourceAdapterUtils.KEY_INSTANCE_ID;
-import static com.google.ads.mediation.ironsource.IronSourceAdapterUtils.TAG;
-import static com.google.ads.mediation.ironsource.IronSourceMediationAdapter.ERROR_AD_ALREADY_LOADED;
-import static com.google.ads.mediation.ironsource.IronSourceMediationAdapter.ERROR_AD_SHOW_UNAUTHORIZED;
-import static com.google.ads.mediation.ironsource.IronSourceMediationAdapter.ERROR_DOMAIN;
-import static com.google.ads.mediation.ironsource.IronSourceMediationAdapter.ERROR_INVALID_SERVER_PARAMETERS;
-import static com.google.ads.mediation.ironsource.IronSourceMediationAdapter.ERROR_REQUIRES_ACTIVITY_CONTEXT;
+import static com.google.ads.mediation.ironsource.IronSourceConstants.DEFAULT_INSTANCE_ID;
+import static com.google.ads.mediation.ironsource.IronSourceConstants.KEY_INSTANCE_ID;
+import static com.google.ads.mediation.ironsource.IronSourceConstants.TAG;
+import static com.google.ads.mediation.ironsource.IronSourceConstants.ERROR_AD_ALREADY_LOADED;
+import static com.google.ads.mediation.ironsource.IronSourceConstants.ERROR_DOMAIN;
+import static com.google.ads.mediation.ironsource.IronSourceConstants.ERROR_INVALID_SERVER_PARAMETERS;
+import static com.google.ads.mediation.ironsource.IronSourceConstants.ERROR_REQUIRES_ACTIVITY_CONTEXT;
 
 import android.app.Activity;
 import android.content.Context;
@@ -40,10 +53,13 @@ public class IronSourceRewardedAd implements MediationRewardedAd {
 
   /**
    * Mediation listener used to forward rewarded ad events from IronSource SDK to Google Mobile Ads
-   * SDK for loading phases of the ad
+   * SDK for loading phases of the ad.
    */
   private final MediationAdLoadCallback<MediationRewardedAd, MediationRewardedAdCallback>
       mediationAdLoadCallback;
+
+  /** IronSource rewarded ad context. */
+  private final Context context;
 
   /** This is the id of the rewarded video instance requested. */
   private final String instanceID;
@@ -55,68 +71,74 @@ public class IronSourceRewardedAd implements MediationRewardedAd {
               mediationAdLoadCallback) {
     Bundle serverParameters = rewardedAdConfiguration.getServerParameters();
     instanceID = serverParameters.getString(KEY_INSTANCE_ID, DEFAULT_INSTANCE_ID);
+    context = rewardedAdConfiguration.getContext();
     this.mediationAdLoadCallback = mediationAdLoadCallback;
-    IronSource.setISDemandOnlyRewardedVideoListener(ironSourceRewardedListener);
   }
 
-	/** Getters and Setters */
-	public static IronSourceRewardedAd getFromAvailableInstances(String instanceId) {
-		return availableInstances.get(instanceId);
-	}
+  /** Getters and Setters. */
+  public static IronSourceRewardedAd getFromAvailableInstances(String instanceId) {
+    return availableInstances.get(instanceId);
+  }
 
-	public static void removeFromAvailableInstances(String instanceId) {
-		availableInstances.remove(instanceId);
-	}
+  public static void removeFromAvailableInstances(String instanceId) {
+    availableInstances.remove(instanceId);
+  }
 
-	public static IronSourceRewardedAdListener getIronSourceRewardedListener(){
-		return ironSourceRewardedListener;
-	}
+  public static IronSourceRewardedAdListener getIronSourceRewardedListener() {
+    return ironSourceRewardedListener;
+  }
 
-	public MediationRewardedAdCallback getRewardedAdCallback(){
-		return mediationRewardedAdCallback;
-	}
+  public MediationRewardedAdCallback getRewardedAdCallback() {
+    return mediationRewardedAdCallback;
+  }
 
-	public void setRewardedAdCallback(MediationRewardedAdCallback adCallback){
-		mediationRewardedAdCallback = adCallback;
-	}
+  public void setRewardedAdCallback(MediationRewardedAdCallback adCallback) {
+    mediationRewardedAdCallback = adCallback;
+  }
 
-	public MediationAdLoadCallback<MediationRewardedAd, MediationRewardedAdCallback> getMediationAdLoadCallback(){
-		return mediationAdLoadCallback;
-	}
+  public MediationAdLoadCallback<MediationRewardedAd, MediationRewardedAdCallback>
+      getMediationAdLoadCallback() {
+    return mediationAdLoadCallback;
+  }
 
-
-	/** Interstitial Manager Section */
-  public void loadRewardedVideo(@NonNull Context context, @NonNull String instanceId) {
-    if (!(context instanceof Activity)) {
-      String errorMessage =
-          ERROR_REQUIRES_ACTIVITY_CONTEXT + "IronSource requires an Activity context to load ads.";
-      AdError contextError =
-          new AdError(ERROR_REQUIRES_ACTIVITY_CONTEXT, errorMessage, ERROR_DOMAIN);
-      onAdFailedToLoad(contextError);
+  public void loadRewardedVideo() {
+    if (!isParamsValid()) {
       return;
     }
 
     Activity activity = (Activity) context;
+    availableInstances.put(instanceID, this);
+    Log.d(TAG, String.format("Loading IronSource rewarded ad with instance ID: %s", instanceID));
+    IronSource.loadISDemandOnlyRewardedVideo(activity, instanceID);
+  }
 
-    if (TextUtils.isEmpty(instanceId)) {
-      AdError loadError =
-          new AdError(
-              ERROR_INVALID_SERVER_PARAMETERS, "Missing or invalid instance ID.", ERROR_DOMAIN);
+  /** Checks if the parameters for loading this instance are valid. */
+  private boolean isParamsValid() {
+    // Check that the context is an Activity.
+    AdError loadError = IronSourceAdapterUtils.checkContextIsActivity(context);
+    if (loadError != null) {
       onAdFailedToLoad(loadError);
-      return;
+      return false;
     }
 
-    if (!canLoadRewardedVideoInstance(instanceId)) {
+    // Check that the instance ID is valid.
+    loadError = IronSourceAdapterUtils.checkInstanceId(instanceID);
+    if (loadError != null) {
+      onAdFailedToLoad(loadError);
+      return false;
+    }
+
+    // Check that an Ad for this instance ID is not already loading.
+    if (!canLoadRewardedVideoInstance(instanceID)) {
       String errorMessage =
-          String.format("An ad is already loading for instance ID: %s", instanceId);
+          String.format(
+              "An IronSource Rewarded ad is already loading for instance ID: %s", instanceID);
       AdError concurrentError = new AdError(ERROR_AD_ALREADY_LOADED, errorMessage, ERROR_DOMAIN);
       onAdFailedToLoad(concurrentError);
-      return;
+      return false;
     }
 
-    availableInstances.put(instanceId, this);
-    Log.d(TAG, String.format("Loading IronSource rewarded ad with instance ID: %s", instanceID));
-    IronSource.loadISDemandOnlyRewardedVideo(activity, instanceId);
+    return true;
   }
 
   private boolean canLoadRewardedVideoInstance(@NonNull String instanceId) {
@@ -124,32 +146,15 @@ public class IronSourceRewardedAd implements MediationRewardedAd {
     return (ironSourceRewardedAd == null);
   }
 
-  /** Rewarded Video show Ad */
+  /** Rewarded Video show Ad. */
   @Override
   public void showAd(@NonNull Context context) {
     Log.d(
         TAG, String.format("Showing IronSource rewarded ad for instance ID: %s", this.instanceID));
-    IronSourceRewardedAd ironSourceRewardedAd = availableInstances.get(this.instanceID);
-    if (ironSourceRewardedAd == null) {
-      AdError showError =
-          new AdError(
-              ERROR_AD_SHOW_UNAUTHORIZED,
-              "IronSource adapter does not have authority to show this instance.",
-              ERROR_DOMAIN);
-      Log.w(TAG, showError.getMessage());
-      if (mediationAdLoadCallback != null) {
-        mediationAdLoadCallback.onFailure(showError);
-      }
-      return;
-    }
-
-    // IronSource may call onRewardedVideoAdRewarded() after onRewardedVideoAdClosed() on some
-    // rewarded ads. Store the adapter reference so callbacks can be forwarded properly regardless
-    // of order.
     IronSource.showISDemandOnlyRewardedVideo(this.instanceID);
   }
 
-  /** Pass Load Fail from IronSource SDK to Google Mobile Ads */
+  /** Pass Load Fail from IronSource SDK to Google Mobile Ads. */
   private void onAdFailedToLoad(@NonNull AdError loadError) {
     Log.w(TAG, loadError.getMessage());
     mediationAdLoadCallback.onFailure(loadError);
