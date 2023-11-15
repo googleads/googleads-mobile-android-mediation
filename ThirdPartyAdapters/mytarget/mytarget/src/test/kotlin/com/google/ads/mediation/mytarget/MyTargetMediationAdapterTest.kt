512a7a1ccfe5e11301a3789e90e80f33609ef26b
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2023-11-15 10:48:28 -0800

    Add unit test for initialize method in MyTarget Android Adapter
    PiperOrigin-RevId: 582732451

--- ThirdPartyAdapters/mytarget/mytarget/src/test/kotlin/com/google/ads/mediation/mytarget/MyTargetMediationAdapterTest.kt
+++ ThirdPartyAdapters/mytarget/mytarget/src/test/kotlin/com/google/ads/mediation/mytarget/MyTargetMediationAdapterTest.kt
@@ -1,24 +1,39 @@
 package com.google.ads.mediation.mytarget
 
+import android.content.Context
+import androidx.core.os.bundleOf
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth.assertThat
+import com.google.ads.mediation.adaptertestkit.mediationAdapterInitializeVerifySuccess
+import com.google.android.gms.ads.mediation.InitializationCompleteCallback
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
 
 /** Class containing unit tests for MyTargetMediationAdapter.java */
 @RunWith(AndroidJUnit4::class)
 class MyTargetMediationAdapterTest {
 
   private var myTargetMediationAdapter: MyTargetMediationAdapter = MyTargetMediationAdapter()
+  private val context = ApplicationProvider.getApplicationContext<Context>()
+  private val mockInitializationCompleteCallback = mock<InitializationCompleteCallback>()
 
   @Before
   fun setUp() {
     myTargetMediationAdapter = MyTargetMediationAdapter()
   }
 
+  // region Initialize Tests
+
   @Test
-  fun instanceOfMyTargetAdapter_returnsAnInstanceOfMyTargetMediationAdapter() {
-    assertThat(myTargetMediationAdapter is MyTargetMediationAdapter).isTrue()
+  fun initialize_invokesOnInitializationSucceeded() {
+    myTargetMediationAdapter.mediationAdapterInitializeVerifySuccess(
+      context,
+      mockInitializationCompleteCallback,
+      /* serverParameters= */ bundleOf()
+    )
   }
+
+  // endregion
 }
