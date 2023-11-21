407d27c2ca2838f0a5cfbc36a308e0e1035872df
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2023-11-20 17:15:17 -0800

    Unit test coverage for Maio Adapter getVersionInfo method.
    PiperOrigin-RevId: 584167800

--- ThirdPartyAdapters/maio/maio/src/test/kotlin/com/google/ads/mediation/maio/MaioMediationAdapterTest.kt
+++ ThirdPartyAdapters/maio/maio/src/test/kotlin/com/google/ads/mediation/maio/MaioMediationAdapterTest.kt
@@ -1,24 +1,50 @@
 package com.google.ads.mediation.maio
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.ads.mediation.adaptertestkit.assertGetVersionInfo
+import com.google.ads.mediation.maio.MaioUtils.getVersionInfo
 import com.google.common.truth.Truth.assertThat
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.mockStatic
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.whenever
 
 /** Class containing unit tests for [MaioMediationAdapter] */
 @RunWith(AndroidJUnit4::class)
 class MaioMediationAdapterTest {
 
-  private var maioMediationAdapter: MaioMediationAdapter = MaioMediationAdapter()
-
-  @Before
-  fun setUp() {
-    maioMediationAdapter = MaioMediationAdapter()
-  }
+  private var adapter: MaioMediationAdapter = MaioMediationAdapter()
 
   @Test
   fun instanceOfMaioAdapter_returnsAnInstanceOfMaioMediationAdapter() {
-    assertThat(maioMediationAdapter is MaioMediationAdapter).isTrue()
+    assertThat(adapter is MaioMediationAdapter).isTrue()
+  }
+
+  @Test
+  fun getVersionInfo_validVersionFor4Digits_returnsTheSameVersion() {
+    mockStatic(MaioUtils::class.java).use {
+      whenever(getVersionInfo()) doReturn "7.3.2.1"
+
+      adapter.assertGetVersionInfo(expectedValue = "7.3.201")
+    }
+  }
+
+  @Test
+  fun getVersionInfo_validVersionFor5Digits_returnsTheValidVersion() {
+    mockStatic(MaioUtils::class.java).use {
+      whenever(getVersionInfo()) doReturn "7.3.2.1.5"
+
+      adapter.assertGetVersionInfo(expectedValue = "7.3.201")
+    }
+  }
+
+  @Test
+  fun getVersionInfo_invalidVersion_returnsZeros() {
+    mockStatic(MaioUtils::class.java).use {
+      whenever(getVersionInfo()) doReturn "3.2"
+
+      adapter.assertGetVersionInfo(expectedValue = "0.0.0")
+    }
   }
 }
