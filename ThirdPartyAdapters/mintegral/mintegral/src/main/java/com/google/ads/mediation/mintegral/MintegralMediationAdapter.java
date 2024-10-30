4cca6af03da7920a8de773a77c07ec7bd499fe3f
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2024-10-30 11:41:03 -0700

    Initialize Mintegral SDK in a background thread (to avoid ANRs).
    PiperOrigin-RevId: 691497491

--- ThirdPartyAdapters/mintegral/mintegral/src/main/java/com/google/ads/mediation/mintegral/MintegralMediationAdapter.java
+++ ThirdPartyAdapters/mintegral/mintegral/src/main/java/com/google/ads/mediation/mintegral/MintegralMediationAdapter.java
@@ -183,21 +183,29 @@ public class MintegralMediationAdapter extends RtbAdapter {
     } catch (Throwable e) {
       e.printStackTrace();
     }
-    mBridgeSDK.init(configurationMap, context, new SDKInitStatusListener() {
-      @Override
-      public void onInitSuccess() {
-        initializationCompleteCallback.onInitializationSucceeded();
-      }
+    // Initialize the Mintegral SDK in a separate thread to avoid blocking the main thread.
+    new Thread(
+            () ->
+                mBridgeSDK.init(
+                    configurationMap,
+                    context,
+                    new SDKInitStatusListener() {
+                      @Override
+                      public void onInitSuccess() {
+                        initializationCompleteCallback.onInitializationSucceeded();
+                      }
 
-      @Override
-      public void onInitFail(String errorMessage) {
-        AdError initError = createSdkError(MintegralConstants.ERROR_CODE_SDK_INIT_FAILED,
-            errorMessage);
-        initializationCompleteCallback.onInitializationFailed(initError.getMessage());
-        Log.w(TAG, initError.toString());
-      }
-    });
-
+                      @Override
+                      public void onInitFail(String errorMessage) {
+                        AdError initError =
+                            createSdkError(
+                                MintegralConstants.ERROR_CODE_SDK_INIT_FAILED, errorMessage);
+                        initializationCompleteCallback.onInitializationFailed(
+                            initError.getMessage());
+                        Log.w(TAG, initError.toString());
+                      }
+                    }))
+        .start();
   }
 
   @Override
