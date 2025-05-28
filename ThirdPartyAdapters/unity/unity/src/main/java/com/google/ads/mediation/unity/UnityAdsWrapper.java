2353448e7b220b37d4cb55ff06857d5ecb37fac4
lmoreault <luc.moreault@unity3d.com>
2025-05-28 14:22:08 -0400

    UnityAds: add AdFormat to getToken API (+1 squashed commit) Squashed commits: [6e14f5b2] UnityAds: add AdFormat to getToken API

--- ThirdPartyAdapters/unity/unity/src/main/java/com/google/ads/mediation/unity/UnityAdsWrapper.java
+++ ThirdPartyAdapters/unity/unity/src/main/java/com/google/ads/mediation/unity/UnityAdsWrapper.java
@@ -2,10 +2,12 @@ package com.google.ads.mediation.unity;
 
 import android.content.Context;
 import com.unity3d.ads.IUnityAdsInitializationListener;
+import com.unity3d.ads.IUnityAdsTokenListener;
+import com.unity3d.ads.TokenConfiguration;
 import com.unity3d.ads.UnityAds;
 import com.unity3d.ads.metadata.MediationMetaData;
 
-/** Wrapper class for {@link UnityAds#initialize} */
+/** Wrapper class for {@link UnityAds} */
 class UnityAdsWrapper {
   public void initialize(Context context, String gameId, IUnityAdsInitializationListener listener) {
     UnityAds.initialize(context, gameId, false, listener);
@@ -22,4 +24,13 @@ class UnityAdsWrapper {
   public MediationMetaData getMediationMetaData(Context context) {
     return new MediationMetaData(context);
   }
+
+  public void getToken(IUnityAdsTokenListener tokenListener) {
+    UnityAds.getToken(tokenListener);
+  }
+
+  public void getToken(TokenConfiguration tokenConfiguration, IUnityAdsTokenListener tokenListener) {
+    UnityAds.getToken(tokenConfiguration, tokenListener);
+  }
+
 }
