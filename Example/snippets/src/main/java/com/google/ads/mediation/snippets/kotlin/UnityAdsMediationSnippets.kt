fcc9bbb6085d8752f5c6ad815737746fdd8cc501
Joshua James Pascual Lagonera <joshuaja@google.com>
2025-09-25 13:39:18 -0700

    Update Java and Kotlin mediation examples for Unity Ads mediation.
    PiperOrigin-RevId: 811483161

---
+++ Example/snippets/src/main/java/com/google/ads/mediation/snippets/kotlin/UnityAdsMediationSnippets.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.ads.mediation.snippets.kotlin
+
+import android.app.Activity
+import com.unity3d.ads.metadata.MetaData
+
+/**
+ * Kotlin code snippets for https://developers.google.com/admob/android/mediation/unity and
+ * https://developers.google.com/ad-manager/mobile-ads-sdk/android/mediation/unity
+ */
+class UnityAdsMediationSnippets : Activity() {
+
+  private fun setGdprMetaData() {
+    // [START set_gdpr_meta_data]
+    val gdprMetaData = MetaData(this)
+    gdprMetaData["gdpr.consent"] = true
+    gdprMetaData.commit()
+    // [END set_gdpr_meta_data]
+  }
+
+  private fun setCcpaMetaData() {
+    // [START set_ccpa_meta_data]
+    val ccpaMetaData = MetaData(this)
+    ccpaMetaData["privacy.consent"] = true
+    ccpaMetaData.commit()
+    // [END set_ccpa_meta_data]
+  }
+}
